eee02a277d0e2b3172d80f5ea92287461aae6688
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-15 14:17:35 +0100

    got elements from the DOM

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -1,1 +1,10 @@
-console.log('OK JS');
+/*
+    # PALIDROMA: SNACK 1.
+    1.  Chiedere all’utente di inserire una parola tramiite un form
+    2. Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+// Prendo gli elementi dal DOM 
+const userWordElement = document.getElementById('user-word');
+const btnCheck = document.getElementById('btn');
+
