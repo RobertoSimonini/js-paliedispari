d3162e9b00eebe9413b5949ae9561d2fbdadb855
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-15 15:31:35 +0100

    excercise 1 completed

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -7,16 +7,48 @@
 // Prendo gli elementi dal DOM 
 const userWordElement = document.getElementById('user-word');
 const btnCheck = document.getElementById('btn');
+const message = document.getElementById('message');
+
+
+
+// Creo una funzione che mi permetta di verificare che la paroala dell'utente sia palindroma 
+
 
 // Aggiungo l'evento al click del bottone 
 btnCheck.addEventListener('click', function(){
     // Prendo il value dell'input 
     const userWord = userWordElement.value.trim();
-
+    
     // ! Validazione 
     if (!userWord) {
-        alert('non hai inserito valori validi.')
+        message.innerText = 'Non hai inserito valori validi.'
         return;
     }
-
+    
+    // Verifico se la parola data dall'utente è palindroma 
+    const userPalindrome = isPalindrome(userWord);
+    
+    if (userPalindrome) {
+        message.innerText = 'La parola fornita è Palindroma'
+    } else {
+        message.innerText = 'La parola fornita non è Palindroma'
+    }   
 });
+
+
+function isPalindrome (word){
+
+    // Trasformo la parola sempre in Lowercase perchè se mette un nome non riesco a confrontarli 
+    word = word.toLowerCase();
+    // creo un array con la parola fornita 
+    const wordCarachters = word.split('');
+    // Creo un ciclo for per fare il confronto
+    for (let i = 0; i < wordCarachters.length; i++){
+    // Creo una condizione per confrontarli 
+        if (wordCarachters[i] !== wordCarachters[wordCarachters.length - 1 - i]) {
+            return false;
+        } else {
+            return true;
+        }
+    }  
+};
