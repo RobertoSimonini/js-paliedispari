53505a3344599f5bfe398a8fa9509a58944964b0
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-15 17:10:35 +0100

    created isEven function

--- Snack 2/js/script.js
+++ Snack 2/js/script.js
@@ -17,11 +17,15 @@ const playBtn = document.getElementById('play-btn');
 const message = document.getElementById('message');
 
 // Creo una funzione che generi un numero random da 1 a 5 per il Computer 
-
 function generatePcRandomnumber () {
     const pcNumber = Math.floor((Math.random()*5) + 1);
     return pcNumber;
 };
+
+// Creo una funzione per verificare che un numero sia pari o dispari 
+function isEven (num) {
+    return num % 2 === 0;
+}
 
 playBtn.addEventListener ('click', function (){
 
